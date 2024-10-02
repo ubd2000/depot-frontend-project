d4411c6c591079a38f4cc2f4d1a5b3668ecbc888
srwoo <selim0915@gmail.com>
2024-10-03 03:17:10 +0900

    layout - footer

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -6,6 +6,7 @@ import {
   product_6, product_7, product_8, product_9, product_10,
   deal_ofthe_week
 } from '../../utils/images.import';
+import Footer from '../../layouts/footer';
 
 const Main: React.FC = () => {
   return (
@@ -761,42 +762,7 @@ const Main: React.FC = () => {
         </div>
       </div>
 
-      {/* <!-- Footer --> */}
-
-      <footer className="footer">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-6">
-              <div className="footer_nav_container d-flex flex-sm-row flex-column align-items-center justify-content-lg-start justify-content-center text-center">
-                <ul className="footer_nav">
-                  <li><a href="#">Blog</a></li>
-                  <li><a href="#">FAQs</a></li>
-                  <li><a href="/contact">Contact us</a></li>
-                </ul>
-              </div>
-            </div>
-            <div className="col-lg-6">
-              <div className="footer_social d-flex flex-row align-items-center justify-content-lg-end justify-content-center">
-                <ul>
-                  <li><a href="#"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
-                  <li><a href="#"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
-                  <li><a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a></li>
-                  <li><a href="#"><i className="fa fa-skype" aria-hidden="true"></i></a></li>
-                  <li><a href="#"><i className="fa fa-pinterest" aria-hidden="true"></i></a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-lg-12">
-              <div className="footer_nav_container">
-                <div className="cr">©2018 All Rights Reserverd. Made with <i className="fa fa-heart-o" aria-hidden="true"></i> by <a href="#">Colorlib</a> &amp; distributed by <a href="https://themewagon.com">ThemeWagon</a></div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </footer>
-
+      <Footer />
     </div>
   )
 }
--- src/pages/product/ProductDetail/index.tsx
+++ src/pages/product/ProductDetail/index.tsx
@@ -3,8 +3,9 @@ import {
   single_1_thumb, single_2_thumb, single_3_thumb,
   desc_1, desc_2, desc_3
 } from '../../../utils/images.import';
+import Footer from '../../../layouts/footer';
 
-const ProductDetail = () => {
+const ProductDetail: React.FC = () => {
   return (
     <div className="super_container">
 
@@ -74,7 +75,7 @@ const ProductDetail = () => {
             <div className="row">
               <div className="col-lg-12 text-right">
                 <div className="logo_container">
-                  <a href="#">colo<span>shop</span></a>
+                  <a href="/">colo<span>shop</span></a>
                 </div>
                 <nav className="navbar">
                   <ul className="navbar_menu">
@@ -472,42 +473,7 @@ const ProductDetail = () => {
         </div>
       </div>
 
-      {/* <!--  Footer --> */}
-
-      <footer className="footer">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-6">
-              <div className="footer_nav_container d-flex flex-sm-row flex-column align-items-center justify-content-lg-start justify-content-center text-center">
-                <ul className="footer_nav">
-                  <li><a href="#">Blog</a></li>
-                  <li><a href="#">FAQs</a></li>
-                  <li><a href="/contact">Contact us</a></li>
-                </ul>
-              </div>
-            </div>
-            <div className="col-lg-6">
-              <div className="footer_social d-flex flex-row align-items-center justify-content-lg-end justify-content-center">
-                <ul>
-                  <li><a href="#"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
-                  <li><a href="#"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
-                  <li><a href="#"><i className="fa fa-instagram" aria-hidden="true"></i></a></li>
-                  <li><a href="#"><i className="fa fa-skype" aria-hidden="true"></i></a></li>
-                  <li><a href="#"><i className="fa fa-pinterest" aria-hidden="true"></i></a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-lg-12">
-              <div className="footer_nav_container">
-                <div className="cr">©2018 All Rights Reserverd. This template is made with <i className="fa fa-heart-o" aria-hidden="true"></i> by <a href="#">Colorlib</a> &amp; distributed by <a href="https://themewagon.com">ThemeWagon</a></div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </footer>
-
+      <Footer />
     </div>
   )
 }
